156a8504a397ea3a80fa6876dacaaf289fc53dc5
yusenw2 <yusenw2@illinois.edu>
2019-12-02 19:36:40 -0600

    first init

---
+++ ThriftyApp/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ThriftyApp
+//
+//  Created by Yusen Wang on 12/2/19.
+//  Copyright © 2019 Yusen Wang. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+
+}
+
